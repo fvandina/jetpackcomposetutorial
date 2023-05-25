d2b3d0e915509e7cf30f8c191f24d90af6e88140
Luis Guzman <laguz81@gmail.com>
2023-05-24 21:52:06 -0500

    Tutorial fin clase 4 animaciones

--- app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -18,6 +19,10 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -28,19 +33,58 @@ import androidx.compose.ui.unit.dp
 import com.example.jetpackcomposetutorial.ui.theme.JetpackComposeTutorialTheme
 
 private val messages: List<MyMessage> = listOf(
-    MyMessage("Hola Jetpack Compose 1!", "Preparado"),
-    MyMessage("Hola Jetpack Compose 2!", "Preparado"),
-    MyMessage("Hola Jetpack Compose 3!", "Preparado"),
-    MyMessage("Hola Jetpack Compose 4!", "Preparado"),
-    MyMessage("Hola Jetpack Compose 5!", "Preparado"),
-    MyMessage("Hola Jetpack Compose 6!", "Preparado"),
-    MyMessage("Hola Jetpack Compose 7!", "Preparado"),
-    MyMessage("Hola Jetpack Compose 8!", "Preparado"),
-    MyMessage("Hola Jetpack Compose 9!", "Preparado"),
-    MyMessage("Hola Jetpack Compose 10!", "Preparado"),
-    MyMessage("Hola Jetpack Compose 11!", "Preparado"),
-    MyMessage("Hola Jetpack Compose 12!", "Preparado"),
-    MyMessage("Hola Jetpack Compose 13!", "Preparado"),
+    MyMessage(
+        "Hola Jetpack Compose 1!",
+        "Preparado 1? Vivamus pellentesque lorem eros, sed porttitor ante scelerisque sit amet. Aliquam erat volutpat. Nullam consectetur maximus neque, at vestibulum leo luctus vitae."
+    ),
+    MyMessage(
+        "Hola Jetpack Compose 2!",
+        "Preparado 2?  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    ),
+    MyMessage(
+        "Hola Jetpack Compose 3!",
+        "Preparado 3? Duis vel ornare nisi, id condimentum quam. Sed nibh ex, faucibus vitae enim mollis, volutpat lacinia tellus. Aenean a convallis justo, at rutrum enim. Curabitur sed ex non ligula tempus varius non commodo nisl. Curabitur fringilla lobortis risus, molestie dictum purus mollis a. "
+    ),
+    MyMessage(
+        "Hola Jetpack Compose 4!",
+        "Preparado 4? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    ),
+    MyMessage(
+        "Hola Jetpack Compose 5!",
+        "Preparado 5? Integer felis arcu, auctor sit amet neque id, congue mattis sem. Morbi elementum a eros non vulputate. Mauris feugiat velit a orci consectetur rutrum id sed massa. Sed augue tellus, laoreet convallis magna vitae, auctor pharetra sapien."
+    ),
+    MyMessage(
+        "Hola Jetpack Compose 6!",
+        "Preparado 6? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    ),
+    MyMessage(
+        "Hola Jetpack Compose 7!",
+        "Preparado 7? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    ),
+    MyMessage(
+        "Hola Jetpack Compose 8!",
+        "Preparado 8? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    ),
+    MyMessage(
+        "Hola Jetpack Compose 9!",
+        "Preparado 9? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    ),
+    MyMessage(
+        "Hola Jetpack Compose 10!",
+        "Preparado 10? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    ),
+    MyMessage(
+        "Hola Jetpack Compose 11!",
+        "Preparado 11? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    ),
+    MyMessage(
+        "Hola Jetpack Compose 12!",
+        "Preparado 12? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    ),
+    MyMessage(
+        "Hola Jetpack Compose 13!",
+        "Preparado 13? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    ),
 
 
     )
@@ -94,25 +138,31 @@ fun MyImage() {
 
 @Composable
 private fun MyTexts(message: MyMessage) {
-    Column(modifier = Modifier.padding(start = 8.dp)) {
+    var expanded by remember { mutableStateOf(false) }
+    Column(modifier = Modifier
+        .padding(start = 8.dp)
+        .clickable {
+            expanded = !expanded
+        }) {
 
         MyText(
             text = message.title,
             MaterialTheme.colorScheme.primary,
-            MaterialTheme.typography.headlineMedium
+            MaterialTheme.typography.headlineSmall
         )
         Spacer(modifier = Modifier.height(16.dp))
         MyText(
             text = message.body,
             MaterialTheme.colorScheme.onBackground,
-            MaterialTheme.typography.labelLarge
+            MaterialTheme.typography.labelLarge,
+            if (expanded) Int.MAX_VALUE else 2
         )
     }
 }
 
 @Composable
-fun MyText(text: String, color: Color, style: TextStyle) {
-    Text(text, color = color, style = style)
+fun MyText(text: String, color: Color, style: TextStyle, lines: Int = Int.MAX_VALUE) {
+    Text(text, color = color, style = style, maxLines = lines)
 }
 
 @Preview(showSystemUi = true)
